eefad22da33964802556c5151004db8f80ac6fad
Nachoxt17 <ignacioceaglio@gmail.com>
2021-01-29 17:27:01 -0300

    4th Commit

--- MyFirstToken.sol
+++ MyFirstToken.sol
@@ -6,15 +6,40 @@ contract NachoCoin {
   uint8 public decimals;
   uint256 public totalSupply;
   mapping(address => uint256) public balanceOf;
-  mapping(address => mapping(address => uint256)) public approve;
+  mapping(address => mapping(address => uint256)) public allowance;
 
-  event Approval(address indexed _owner, address indexed _spender, uint256 _value)
+  constructor() public {
+    name = "NachoCoin";
+    symbol = "NC";
+    decimals = 8;
+    totalSupply = 999999999 * (uint256(10) ** decimals);
+    balanceOf[msg.sender] = totalSupply;
+  }
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
   function transfer(address _to, uint256 _value) public returns (bool success) {
     require(balanceOf[msg.sender] >= _value);
     balanceOf[msg.sender] -= _value;
     balanceOf[_to] += _value;
-    emit event Approval( msg.sender, _to,  _value);
+    emit Transfer( msg.sender, _to,  _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, uint256 _value);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[_from] >= _value);
+    require(allowance[_from][msg.sender] >= _value);
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+    allowance[_from][msg.sender] -= _value;
+    emit Transfer(_from, _to,  _value);
     return true;
   }
 }
